cdadb4714a2f139449e09d00d06d25100ae4b5ba
Charun <uparacha@grinnell.edu>
2019-01-31 20:10:33 -0600

    format Caesar

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -3,41 +3,38 @@ import java.io.PrintWriter;
 public class CaesarCipher {
 
   /*
-  Procedure:
-    main
-  Parameters:
-    args, a String array
-  Purpose:
-    To receive inputs from the command line and call the appropriate procedures
-  Produces:
-    Error codes, if the inputs are incorrect
-  Preconditions:
-    args[0] has to be "encode" or "decode"
-    args has to be of length 2
-  Postcondtitions:
-    Exits with error code 1 or 2
-    Calls the appropriate function and produces a list of encoded/decoded text
+   * Procedure: 
+   *    main
+   * Parameters: 
+   *    args, a String array 
+   * Purpose: 
+   *    To receive inputs from the command
+   *    line and call the appropriate procedures
+   * Produces: 
+   *    Error codes, if the inputs are incorrect
+   * Preconditions: 
+   *    args[0] has to be "encode" or "decode" args has to be of length 2
+   * Postcondtitions: 
+   *    Exits with error code 1 or 2 Calls the appropriate function and produces a
+   *    list of encoded/decoded text
    */
   public static void main(String[] args) throws Exception {
-  
-    if (args.length != 2)
-    {
+
+    if (args.length != 2) {
       System.err.println("Incorrect number of parameters. ");
       System.exit(2);
     }
     String action = args[0];
     String input = args[1];
-    if (input.isEmpty())
-    {
+    if (input.isEmpty()) {
       System.err.println("Input is empty");
       System.ext(1);
     }
-    if (action.equals("encode") == false || action.equals("encode") == false)
-    {
+    if (action.equals("encode") == false || action.equals("encode") == false) {
       System.err.println("Invalid command. Please enter \"encode\" or \"decode\".");
       System.exit(1);
     }
-    
+
     char[] str = input.toCharArray();
     if (action.equals("encode")) {
       encode(str);
@@ -46,20 +43,22 @@ public class CaesarCipher {
       decode(str);
     }
   }
-  
+
   /*
-    Procedure:
-      encode
-    Parameters:
-      str, a character array
-    Produces:
-      A list of text encoded with all 26 permutations of the Caesar Cipher
-    Purpose:
-      Encode the plaintext using Caesar Cipher
-    Preconditions:
-      str should not be empty
-    Postconditions:
-      A list of 26 encoded text should be printed to the screen
+   * Procedure:
+   *    encode 
+   * Parameters:
+   *    str, a character array 
+   * Produces: 
+   *    A list of text encoded with all
+   *    26 permutations of the Caesar Cipher 
+   * Purpose: 
+   *    Encode the plaintext using Caesar Cipher
+   * Preconditions:
+   *    str should not be empty 
+   * Postconditions: 
+   *    A list of 26 encoded text should be
+   *    printed to the screen
    */
 
   public static void encode(char[] str) {
@@ -82,18 +81,20 @@ public class CaesarCipher {
   }
 
   /*
-    Procedure:
-      decode
-    Parameters:
-      str, a character array
-    Produces:
-      A list of text decoded with all 26 permutations of the Caesar Cipher
-    Purpose:
-      Decode the encrypted text using Caesar Cipher
-    Preconditions:
-      str should not be empty
-    Postconditions:
-      A list of 26 decoded text should be printed to the screen
+   * Procedure:
+   *    decode 
+   * Parameters:
+   *    str, a character array 
+   * Produces:
+   *    A list of text decoded with all
+   *    26 permutations of the Caesar Cipher 
+   * Purpose: 
+   *    Decode the encrypted text using Caesar Cipher
+   * Preconditions: 
+   *    str should not be empty 
+   * Postconditions: 
+   *    A list of 26 decoded text should be
+   *    printed to the screen
    */
 
   public static void decode(char[] str) {
